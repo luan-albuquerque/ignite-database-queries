1bc257a47a4d4bff79fcf2b16ad5fd4c41aca8d0
Luan Albuquerque <luan.santos6605@gmail.com>
2022-11-20 16:23:22 -0400

    desafio ignite-database-queries

---
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,44 @@
+import { getRepository, Repository } from "typeorm";
+
+import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from "../../dtos";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async findUserWithGamesById({
+    user_id,
+  }: IFindUserWithGamesDTO): Promise<User> {
+    const user = await this.repository.findOne({
+      where: { id: user_id },
+      relations: ["games"],
+    });
+
+    if (!user) {
+      throw new Error("User does not exist");
+    }
+
+    return user;
+  }
+
+  async findAllUsersOrderedByFirstName(): Promise<User[]> {
+    const query = "SELECT * FROM users ORDER BY first_name ASC";
+
+    return await this.repository.query(query); // Complete usando raw query
+  }
+
+  async findUserByFullName({
+    first_name,
+    last_name,
+  }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
+    return await this.repository.query(
+      "SELECT * FROM users WHERE first_name ILIKE $1 AND last_name ILIKE $2",
+      [first_name, last_name]
+    ); // Complete usando raw query
+  }
+}
